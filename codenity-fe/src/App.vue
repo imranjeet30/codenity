8bbe999e880edd0eea162daa85fe46002314f392
Ranjeet <you@example.com>
2025-05-24 13:48:58 +0530

    added home page and theme

---
+++ codenity-fe/src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Layout from './components/Layout.vue'
+</script>
+
+<template>
+  <Layout />
+</template>
---
+++ codenity-fe/src/components/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="min-h-screen flex flex-col">
+    <Header />
+    
+    <main class="flex-1 p-4 bg-gray-100">
+      <Home />
+    </main>
+    
+    <Footer />
+  </div>
+</template>
+
+<script setup>
+import Header from './Header.vue'
+import Footer from './Footer.vue'
+import Home from '../views/Home.vue'
+</script>
